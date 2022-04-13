8f819375f00f3dabd3cb5e4c09fe3496fbe945e1
RJ0411 <99846952+RJ0411@users.noreply.github.com>
2022-04-13 17:10:35 +0530

    Update register.js

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -25,7 +25,7 @@ const Register = () => {
     const register = () => {
         const { name, email, password, reEnterPassword } = user
         if( name && email && password && (password === reEnterPassword)){
-            axios.post("http://localhost:9002/register", user)
+            axios.post("https://4c66-103-104-225-230.ngrok.io/register", user)
             .then( res => {
                 alert(res.data.message)
                 history.push("/login")
